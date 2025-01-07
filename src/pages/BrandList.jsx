95ed63f17a66e5a3c1d1527faaf357271e48c5ec
ayathree <nobanitaayathree333@gmail.com>
2025-01-08 01:57:37 +0600

    design the navbar

--- src/pages/BrandList.jsx
+++ src/pages/BrandList.jsx
@@ -2,7 +2,10 @@
 
 const BrandList = () => {
     return (
-        <div>
+        <div className="space-y-20">
+            <div className="mt-10">
+                <p className="text-center mb-5 font-semibold text-4xl uppercase ">Brand List</p>
+            </div>
             
         </div>
     );
